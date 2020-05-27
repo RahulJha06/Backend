7eba4800f18b1983b465994f3e00b08bff1b228d
Rahul Jha <raulaj468@gmail.com>
2020-05-27 18:59:57 +0530

    node example

---
+++ index.js
@@ -0,0 +1,18 @@
+var rect = {
+  perimeter: (x,y) => (2*(x+y)),
+  area: (x,y) => (x*y)
+};
+
+function solveRectangle(l,b){
+  console.log("Solving for rectangle with l = "+l+ "and breadth ="+b);
+  if( l <=0 || b<= 0  ){
+    console.log("Rectangle invlaid");
+  }
+  else{
+    console.log("Area of the rectangel is:" +rect.area(l,b));
+    console.log("Perimeter of the rectange is :" + rect.perimeter(l,b));
+  }
+}
+
+solveRectangle(2,5);
+solveRectangle(4,0);
